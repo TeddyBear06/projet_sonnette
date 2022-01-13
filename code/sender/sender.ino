42c8d356d0ed63ed9e85041c4068b41dfcc1268c
T3d <TeddyBear06@users.noreply.github.com>
2022-01-13 15:31:49 +0100

    Update sender.ino

--- code/sender/sender.ino
+++ code/sender/sender.ino
@@ -9,6 +9,10 @@
 #define BTN     A5
 #define LED_BTN A4
 #define SPK     7
+#define VOLT_INPUT A0
+#define LED_VERTE A1
+#define LED_ORANG A2
+#define LED_ROUGE A3
 
 struct DATA {
   int State;
@@ -20,6 +24,7 @@ SoftwareSerial ESerial(PIN_RX, PIN_TX);
 EBYTE Transceiver(&ESerial, PIN_M0, PIN_M1, PIN_AX);
 
 void setup() {
+  Serial.begin(9600);
   ESerial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(BTN, INPUT_PULLUP);
@@ -27,10 +32,14 @@ void setup() {
   digitalWrite(LED_BTN, LOW);
   pinMode(SPK, OUTPUT);
   digitalWrite(SPK, LOW);
+  pinMode(LED_VERTE, OUTPUT);
+  pinMode(LED_ORANG, OUTPUT);
+  pinMode(LED_ROUGE, OUTPUT);
   Transceiver.init();
 }
 
 void loop() {
+  checkVoltage();
   if (digitalRead(BTN) == HIGH){
     Message.State = 1;
     Transceiver.SendStruct(&Message, sizeof(Message));
@@ -42,6 +51,30 @@ void loop() {
     }
   } else {
     digitalWrite(LED_BTN, LOW);
+  }
+}
+
+void checkVoltage() {
+  int raw_volt_input_val;
+  int input_voltage;
+  float temp;
+  raw_volt_input_val = analogRead(VOLT_INPUT);
+  temp = raw_volt_input_val/4.092;
+  raw_volt_input_val = (int) temp;
+  input_voltage = ((raw_volt_input_val%100)/10);
+  Serial.println(input_voltage);
+  if (input_voltage <= 4) {
+    digitalWrite(LED_VERTE, LOW);
+    digitalWrite(LED_ORANG, LOW);
+    digitalWrite(LED_ROUGE, HIGH);
+  } else if (input_voltage > 3 && input_voltage <= 4) {
+    digitalWrite(LED_VERTE, LOW);
+    digitalWrite(LED_ORANG, HIGH);
+    digitalWrite(LED_ROUGE, LOW);
+  } else {
+    digitalWrite(LED_VERTE, HIGH);
+    digitalWrite(LED_ORANG, LOW);
+    digitalWrite(LED_ROUGE, LOW);
   }
 }
 
